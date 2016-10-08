408812d853930b82d16626ed5ca45c17a69a46b3
Olga Lapanovich <olapanovich@klika-tech.com>
2016-10-08 21:19:56 +0300

    Add task 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,56 +6,63 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please, enter your current salary");
-        int currentSalary = scanner.nextInt();
+        String finishSalary = new String();
 
-        System.out.println("Please, enter the period");
-        int period = scanner.nextInt();
+        do {
 
-        System.out.println("Select the loop:\n 1 - while\n 2 - do ... while\n 3 - for");
-        int loop = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please, enter your current salary");
+            int currentSalary = scanner.nextInt();
 
-        int index = 0;
+            System.out.println("Please, enter the period");
+            int period = scanner.nextInt();
 
+            System.out.println("Select the loop:\n 1 - while\n 2 - do ... while\n 3 - for");
+            int loop = scanner.nextInt();
 
-        switch (loop) {
-            case 1: {
-                System.out.println("WHILE\n");
-                while (period > 0) {
-                    currentSalary += 1000;
-                    period--;
-                    index++;
-                    System.out.printf("%d month: ", index);
-                    System.out.printf("salalry %d \n", currentSalary);
+            int index = 0;
+
+            switch (loop) {
+                case 1: {
+                    System.out.println("WHILE\n");
+                    while (period > 0) {
+                        currentSalary += 1000;
+                        period--;
+                        index++;
+                        System.out.printf("%d month: ", index);
+                        System.out.printf("salalry %d \n", currentSalary);
+                    }
+                    break;
                 }
-                break;
+
+                case 2: {
+                    System.out.println("DO WHILE\n");
+                    do {
+                        currentSalary += 1000;
+                        period--;
+                        index++;
+                        System.out.printf("%d month: ", index);
+                        System.out.printf("salalry %d \n", currentSalary);
+                    }
+                    while (period > 0);
+                    break;
+                }
+
+                default: {
+                    System.out.println("FOR\n");
+                    for (int i = 0; i < period; i++) {
+                        currentSalary += 1000;
+                        System.out.printf("%d month: ", i + 1);
+                        System.out.printf("salalry %d \n", currentSalary);
+                    }
+                    break;
+                }
             }
 
-            case 2: {
-                System.out.println("DO WHILE\n");
-                do {
-                    currentSalary += 1000;
-                    period--;
-                    index++;
-                    System.out.printf("%d month: ", index);
-                    System.out.printf("salalry %d \n", currentSalary);
-                }
-                while (period > 0);
-                break;
-            }
-
-            default: {
-                System.out.println("FOR\n");
-                for (int i = 0; i < period; i++) {
-                    currentSalary += 1000;
-                    System.out.printf("%d month: ", i + 1);
-                    System.out.printf("salalry %d \n", currentSalary);
-                }
-                break;
-            }
-
+            System.out.println("Print 'close' for exit or print any other string to continue");
+            finishSalary = scanner.next();
         }
+        while (!finishSalary.contentEquals("close"));
 
     }
 }
